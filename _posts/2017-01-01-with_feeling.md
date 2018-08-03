ab1c234a271449aeeb93bdb539674bf8243d4f51
Hugh Kennedy <hughskennedy@gmail.com>
2018-08-03 01:37:28 +0100

    Chipping away at the template

--- _posts/2017-01-01-with_feeling.md
+++ _posts/2017-01-01-with_feeling.md
@@ -1,9 +1,7 @@
 ---
-title: Dreams
-subtitle: Website Design
+title: With Feeling!
+date: 2017-01-01
 layout: default
-modal-id: 1
-date: 2014-07-18
 img: dreams.png
 thumbnail: dreams-thumbnail.png
 alt: image-alt
@@ -11,5 +9,5 @@ project-date: April 2014
 client: Start Bootstrap
 category: Web Development
 description: Lorem ipsum dolor sit amet, usu cu alterum nominavi lobortis. At duo novum diceret. Tantas apeirian vix et, usu sanctus postulant inciderint ut, populo diceret necessitatibus in vim. Cu eum dicam feugiat noluisse.
-
+small: true
 ---
---
+++ js/shuffle-items.js
@@ -0,0 +1,34 @@
+shuffle('.col-md-4.portfolio-item');
+shuffle('.col-md-2.portfolio-item');
+shuffle('.team-member', function (node) {
+  return node.parentNode;
+});
+
+function shuffle (query, mapping) {
+  var results = document.querySelectorAll(query);
+  var shuffled = [];
+
+  for (var i = 0; i < results.length; i++) {
+    shuffled[i] = mapping ? mapping(results[i]) : results[i];
+  }
+
+  shuffleArray(shuffled);
+
+  var root = shuffled[0].parentNode;
+  for (var i = shuffled.length - 1; i > 0; i--) {
+    root.insertBefore(shuffled[i], shuffled[0]);
+  }
+}
+
+function shuffleArray(array) {
+  var tmp, current, top = array.length;
+
+  if(top) while(--top) {
+    current = Math.floor(Math.random() * (top + 1));
+    tmp = array[current];
+    array[current] = array[top];
+    array[top] = tmp;
+  }
+
+  return array;
+}
